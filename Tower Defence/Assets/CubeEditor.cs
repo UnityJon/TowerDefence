e9b914483c5b9af51ba2d9c95d85c0402c84ecc1
Jon Woodall <jon@synsidmf.com>
2018-04-23 00:04:01 +0100

    Text label on top of each cube now reports coords of the cube in editor.

--- Tower Defence/Assets/CubeEditor.cs
+++ Tower Defence/Assets/CubeEditor.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnapToGrid : MonoBehaviour {
+public class CubeEditor : MonoBehaviour {
 
 
     [SerializeField] [Range(1f, 20f)] float gridSize=10f;
+    TextMesh textMesh;
 
     // Use this for initialization
     void Start () {
-		
 	}
 
     void Awake()
@@ -26,5 +26,7 @@ public class EditorSnapToGrid : MonoBehaviour {
         snapPos.y = 0f;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = snapPos;
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
     }
 }
