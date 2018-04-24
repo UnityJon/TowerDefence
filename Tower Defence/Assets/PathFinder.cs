95912966040e8bfe945c0cd0bdb53abc51fc6cba
Jon-Desktop <jon@synsidmf.com>
2018-04-24 14:09:13 +0100

    Added and fill dictionary with all blocks.  Runtime check (and warning) for multiple blocks at same location.

---
+++ Tower Defence/Assets/PathFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+
+    Dictionary<Vector2Int, Waypoint> grid=new Dictionary<Vector2Int, Waypoint>();
+
+	// Use this for initialization
+	void Start () {
+        LoadBlocks();
+	}
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            Vector2Int gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Overlapping Block at " + gridPos);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + "Blocks");
+    }
+}
