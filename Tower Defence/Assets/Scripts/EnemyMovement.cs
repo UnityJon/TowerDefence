dec96ebc64929d79ff206d30a7d7afbb8957c6ee
Jon Woodall <jon@synsidmf.com>
2018-04-28 10:21:10 +0100

    Made proper scene with route and applied proper enemy.

--- Tower Defence/Assets/Scripts/EnemyMovement.cs
+++ Tower Defence/Assets/Scripts/EnemyMovement.cs
@@ -14,14 +14,11 @@ public class EnemyMovement : MonoBehaviour {
 
     IEnumerator FollowPath(List<Waypoint> path)
     {
-        print("Starting Patrol");
         foreach (Waypoint waypoint in path)
         {
             transform.position = waypoint.transform.position;
-            print("Visiting " + waypoint.name);
             yield return new WaitForSeconds(1f);
         }
-        print("Ending Patrol");
     }
 
 }
